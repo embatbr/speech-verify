733899c7645afe93e0acf17fbf2e8362158785b8
Eduardo Tenorio <embatbr@gmail.com>
2014-09-03 19:27:34 -0300

    Simplify.
    Signed-off-by: Eduardo Tenorio <embatbr@gmail.com>

--- proj-python/src/preproc.py
+++ proj-python/src/preproc.py
@@ -54,49 +54,34 @@ if __name__ == '__main__':
         os.mkdir('tests/preproc')
     preproc = open('tests/preproc.out', 'w')
 
-    if len(sys.argv) > 1:
-        wave = read_utterance('corpus/%s/' % sys.argv[1], sys.argv[2], '%s_16k.wav' % sys.argv[3])
-        print(wave.length())
-        energy = energy(wave, frame_len=0.01)
-        print(energy, file=preproc)
-        plt.clf()
-        plt.grid(True)
-        plt.plot(wave.data, 'r')
-        plt.savefig('tests/preproc/%s.%s.%s_16k.signal.png' % (sys.argv[1], sys.argv[2], sys.argv[3]))
-        plt.clf()
-        plt.grid(True)
-        plt.plot(energy, 'b')
-        plt.savefig('tests/preproc/%s.%s.%s_16k.energy_dB.png' % (sys.argv[1], sys.argv[2], sys.argv[3]))
-    else:
-        corpus = ['enroll_1', 'enroll_2', 'imposter']
-        for subcorpus in corpus:
-            subcorpus_path = 'corpus/%s/' % subcorpus
-            print(subcorpus_path)
-            print(subcorpus_path, file=preproc)
-            (females, males) = read_speakers(subcorpus_path)
-            speakers = females + males
+    corpus = ['enroll_1', 'enroll_2', 'imposter']
+    for subcorpus in corpus:
+        subcorpus_path = 'corpus/%s/' % subcorpus
+        print(subcorpus_path)
+        print(subcorpus_path, file=preproc)
+        (females, males) = read_speakers(subcorpus_path)
+        speakers = females + males
 
-            for speaker in speakers:
-                print(speaker)
-                print(speaker, file=preproc)
-                utterances = read_utterances_from_speaker(subcorpus_path, speaker)
-                for utterance in utterances:
-                    print(utterance, file=preproc)
-                    wave = read_utterance(subcorpus_path, speaker, utterance)
-                    wave_energy = energy(wave)
-                    print(wave_energy, file=preproc)
+        for speaker in speakers:
+            print(speaker)
+            print(speaker, file=preproc)
+            utterances = read_utterances_from_speaker(subcorpus_path, speaker)
+            for utterance in utterances:
+                print(utterance, file=preproc)
+                wave = read_utterance(subcorpus_path, speaker, utterance)
+                wave_energy = energy(wave)
+                print(wave_energy, file=preproc)
 
-                    if len(sys.argv) > 1 and sys.argv[1] == 'draw':
-                        plt.clf()
-                        plt.grid(True)
-                        plt.plot(wave.data, 'r')
-                        plt.savefig('tests/preproc/%s-%s-%s-signal.png' % (subcorpus, speaker, utterance))
-                        plt.clf()
-                        plt.grid(True)
-                        plt.plot(wave_energy, 'b')
-                        plt.savefig('tests/preproc/%s-%s-%s-energy.png' % (subcorpus, speaker, utterance))
+                plt.clf()
+                plt.grid(True)
+                plt.plot(wave.data, 'r')
+                plt.savefig('tests/preproc/%s-%s-%s-signal.png' % (subcorpus, speaker, utterance))
+                plt.clf()
+                plt.grid(True)
+                plt.plot(wave_energy, 'b')
+                plt.savefig('tests/preproc/%s-%s-%s-energy.png' % (subcorpus, speaker, utterance))
 
-            print(file=preproc)
-            print(file=preproc)
+        print(file=preproc)
+        print(file=preproc)
 
     print('output to file "tests/preproc.out"')
