4d9dc2790205dc01ad6acd710fcbba5ca3d60f4c
Eduardo Tenorio <embatbr@gmail.com>
2014-09-15 20:41:04 -0300

    VAD partially implemented.
    Signed-off-by: Eduardo Tenorio <embatbr@gmail.com>

--- proj-python/src/preproc.py
+++ proj-python/src/preproc.py
@@ -26,9 +26,8 @@ def frame_energy(wave, sample_len, index):
 
     energy = 0
     for sample in samples:
-        energy = energy + np.absolute(sample)  #magnitude instead of square explained in [1]
+        energy = energy + np.absolute(sample)  #'magnitude' instead of 'square' explained in [1]
 
-    energy = 10*np.log10(energy)
     return energy
 
 def energy(wave, frame_len=0.01):
@@ -79,8 +78,87 @@ def zcr(wave, frame_len=0.01):
 
     return zcr
 
-def vad(wave, threshold): #VAD energy + zcr
-    pass
+def search_point(index, energy, itl, itu, incr=1):
+    """Search for the beginning point (if incr == 1) or the ending point (if
+    incr == -1).
+    """
+    m = index
+    while True:
+        while energy[m] < itl:
+            m = m + incr
+
+        i = m
+
+        while (energy[i] >= itl) and (energy[i] < itu): #quebra em f00/phrase02, i = 10 (i == len)
+            i = i + incr
+
+        if energy[i] < itl:
+            m = i + incr
+        else:   #if "energy[i] >= itl", then "energy[i] >= itu"
+            if i == m:
+                return (i - incr)
+            return i
+
+
+def vad(wave, frame_len=0.01): #VAD energy + zcr
+    wave_energy = energy(wave)
+    wave_zcr = zcr(wave)
+
+    initial_silence_len = math.ceil(0.1/frame_len)  #number of frames for the first 100 ms
+
+    #zero crossing rate
+    zcr_avg = np.average(wave_zcr[ : initial_silence_len])
+    zcr_std = np.std(wave_zcr[ : initial_silence_len])
+    fixed_izct = 25     #25 crossings for a frame of 10 ms
+    izct = min(fixed_izct, zcr_avg + 2*zcr_std) #zero crossing threshold
+
+    #energy
+    energy_initial_silence = wave_energy[ : initial_silence_len]
+    peak_energy = np.amax(energy_initial_silence)
+    silence_energy = np.amin(energy_initial_silence)
+    I1 = 0.03*(peak_energy - silence_energy) + silence_energy
+    I2 = 4*silence_energy
+    itl = min(I1, I2)   #lower energy threshold
+    itu = 5*itl         #higher energy threshold
+
+    #beginning point
+    N1 = search_point(0, wave_energy[ : initial_silence_len], itl, itu)
+    print('N1 =', N1)
+
+    #ending point
+    N2 = search_point(initial_silence_len - 1, wave_energy[ : initial_silence_len],
+                      itl, itu, incr=-1)
+    print('N2 =', N2)
+
+    #TODO cortar o lixo
+
+
+def draw(wave):
+    """Draws the graphics of the signal,, energy, energy in dB and zero crossing rate.
+    """
+    wave_energy = energy(wave)
+    wave_zcr = zcr(wave)
+
+    plt.clf()
+    plt.suptitle('SIGNAL')
+    plt.grid(True)
+    plt.plot(wave.data, 'b')
+    plt.savefig('corpus_%s/%s/%s/%s.png' % (command, subcorpus, speaker, utterance))
+    plt.clf()
+    plt.suptitle('ENERGY')
+    plt.grid(True)
+    plt.plot(wave_energy, 'r')
+    plt.savefig('corpus_%s/%s/%s/%s-energy.png' % (command, subcorpus, speaker, utterance))
+    plt.clf()
+    plt.suptitle('ENERGY (dB)')
+    plt.grid(True)
+    plt.plot(10*np.log10(wave_energy), 'm')
+    plt.savefig('corpus_%s/%s/%s/%s-energy-dB.png' % (command, subcorpus, speaker, utterance))
+    plt.clf()
+    plt.suptitle('ZERO CROSSING RATE')
+    plt.grid(True)
+    plt.plot(wave_zcr, 'g')
+    plt.savefig('corpus_%s/%s/%s/%s-zcr.png' % (command, subcorpus, speaker, utterance))
 
 
 if __name__ == '__main__':
@@ -94,7 +172,7 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print('write something you fool!')
         sys.exit()
-    command = sys.argv[1]   # 'draw', 'vad'
+    command = sys.argv[1]   # 'vad', 'draw'
 
     if os.path.exists('corpus_%s' % command):
         shutil.rmtree('corpus_%s' % command)
@@ -117,24 +195,10 @@ if __name__ == '__main__':
             for utterance in utterances:
                 print(utterance)
                 wave = read_utterance(subcorpus_path, speaker, utterance)
-                wave_energy = energy(wave)
-                wave_zcr = zcr(wave)
 
-                if command == 'draw':
-                    plt.clf()
-                    plt.suptitle('SIGNAL')
-                    plt.grid(True)
-                    plt.plot(wave.data, 'r')
-                    plt.savefig('corpus_%s/%s/%s/%s.png' % (command, subcorpus, speaker, utterance))
-                    plt.clf()
-                    plt.suptitle('ENERGY (dB)')
-                    plt.grid(True)
-                    plt.plot(wave_energy, 'b')
-                    plt.savefig('corpus_%s/%s/%s/%s-energy.png' % (command, subcorpus, speaker, utterance))
-                    plt.clf()
-                    plt.suptitle('ZERO CROSSING RATE')
-                    plt.grid(True)
-                    plt.plot(wave_zcr, 'g')
-                    plt.savefig('corpus_%s/%s/%s/%s-zcr.png' % (command, subcorpus, speaker, utterance))
+                if command == 'vad':
+                    vad(wave)
+                elif command == 'draw':
+                    draw(wave)
 
     print('finished')
